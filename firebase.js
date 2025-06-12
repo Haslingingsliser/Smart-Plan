eeeae4e7367b0348d7b63039ef48322c6bf776ca
Haslingingsliser <haidarazfa25@gmail.com>
2025-06-12 16:47:24 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2TnzeIu4bYewXBTxcMR41DiTqCja53_Q",
+  authDomain: "iot-haidar.firebaseapp.com",
+  databaseURL: "https://iot-haidar-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "iot-haidar",
+  storageBucket: "iot-haidar.appspot.com",
+  messagingSenderId: "863414396307",
+  appId: "1:863414396307:web:18900ca6dfb76903343466",
+  measurementId: "G-CC1L1H1Y0L"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+window.realtimeDB = db;
+window.onValue = onValue;
+window.ref = ref;
