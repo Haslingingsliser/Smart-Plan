ecc8e76b0ed3c2c7fc6893c07ce1afed53c66e15
Haslingingsliser <haidarazfa25@gmail.com>
2025-06-12 16:23:24 +0700

    Update save_data.php

--- save_data.php
+++ save_data.php
@@ -1,18 +1,14 @@
 <?php
-// Ambil data dari ESP32
 $suhu = $_GET['suhu'] ?? '';
 $kelembapan = $_GET['kelembapan'] ?? '';
 $tanah = $_GET['tanah'] ?? '';
 
-// Susun sebagai array
 $data = array(
   "suhu" => $suhu,
   "kelembapan" => $kelembapan,
   "tanah" => $tanah
 );
 
-// Ubah ke format JSON dan simpan ke file
 file_put_contents("data.json", json_encode($data));
-
 echo "Data berhasil disimpan!";
 ?>
